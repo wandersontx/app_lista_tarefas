0944329513f8d481db0ffd845f4e7f5a3a64e515
wandersontx <wandersonh89@gmail.com>
2021-01-30 15:45:51 -0300

    Salvando e recuperando dados em arquivos

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  List _listaTarefas = ['Ir ao mercado', 'Estudar', 'Exercício do dia'];
+  List _listaTarefas = [];
+
+  Future<File> _getFile() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarArquivo() async {
+    Map<String, dynamic> tarefa = Map();
+    var arquivo = await _getFile();
+    tarefa['titulo'] = 'Ir ao mercado';
+    tarefa['realizada'] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+  }
+
+  _lerArquivo() async {
+    try {
+      var arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      print('Erro ao recuperar dados: ' + e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _lerArquivo().then((dados) {
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    //_salvarArquivo();
+    print('itens: ' + _listaTarefas.toString());
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
@@ -52,7 +96,7 @@ class _HomeState extends State<Home> {
                 itemCount: _listaTarefas.length,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text(_listaTarefas[index]),
+                    title: Text(_listaTarefas[index]['titulo']),
                   );
                 }),
           ),
